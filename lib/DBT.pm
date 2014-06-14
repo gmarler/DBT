03483dde8e5e7855a90234ab967dbfbcd178284b
Gordon Marler <gmarler@gmarler.com>
2014-06-14 13:28:12 -0400

    initial checkin

--- lib/DBT.pm
+++ lib/DBT.pm
@@ -3,4 +3,27 @@ use warnings;
 
 package DBT;
 
+our $VERSION = '0.001';
+
 1;
+
+=head1 NAME
+
+DBT - My DBIx::Class::Migration tutorial application
+
+=head1 AUTHOR
+
+Gordon Marler L<email:gmarler@gmarler.com>
+
+=head1 SEE ALSO
+
+L<DBIx::Class::Migration>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2014, Gordon Marler L<email:gmarler@gmarler.com>
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
